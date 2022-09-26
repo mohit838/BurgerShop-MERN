c5773be41602d8c6b66764a29ee8a51fcd39325b
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-09-26 19:22:38 +0600

    Initial Header Complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/layout/Header";
 import "./styles/app.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
 
 function App() {
   return (
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { IoFastFoodSharp } from "react-icons/io5";
+import { IoBagOutline } from "react-icons/io5";
+import { IoPersonOutline } from "react-icons/io5";
+import { FiLogIn } from "react-icons/fi";
+import { motion } from "framer-motion";
 
-const Header = () => {
+const Header = ({ isUserAuth = false }) => {
   return (
     <nav>
-      <div>
+      <motion.div initial={{ x: "-100%" }} whileInView={{ x: "0" }}>
         <IoFastFoodSharp />
-      </div>
+      </motion.div>
       <div>
         <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+        <Link to="/cart">
+          <IoBagOutline />
+        </Link>
+
+        <Link to={isUserAuth ? "/me" : "/login"}>
+          {isUserAuth ? <IoPersonOutline /> : <FiLogIn />}
+        </Link>
       </div>
     </nav>
   );
