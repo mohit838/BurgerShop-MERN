ef444e184794f40d8f29c0a18877d22d4558952b
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-09-26 20:42:26 +0600

    Working with home hero

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div>
+        <h1>Burger Shop</h1>
+        <p>Give Yourself A Tasty Treat</p>
+      </div>
+      <a href="#menu">Explore Your Treat</a>
+    </section>
+  );
 };
 
 export default Home;
