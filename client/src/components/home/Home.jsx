7ea0bdc6f392c2048c25b3d76f1dba998f2f6101
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-09-27 10:43:00 +0600

    Home Motions

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Home = () => {
+  const motionOp = {
+    initial: { x: "-100%", opacity: "0" },
+    whileInView: { x: "0", opacity: "1" },
+  };
+
   return (
     <section className="home">
       <div>
-        <h1>Burger Shop</h1>
-        <p>Give Yourself A Tasty Treat</p>
+        <motion.h1 {...motionOp}>Burger Shop</motion.h1>
+        <motion.p {...motionOp} transition={{ delay: "0.2" }}>
+          Give Yourself A Tasty Treat
+        </motion.p>
       </div>
-      <a href="#menu">Explore Your Treat</a>
+      <motion.a
+        href="#menu"
+        initial={{ y: "-100%", opacity: "0" }}
+        whileInView={{ y: "0", opacity: "1" }}
+        transition={{ delay: "0.4" }}
+      >
+        Explore Your Treat
+      </motion.a>
     </section>
   );
 };
