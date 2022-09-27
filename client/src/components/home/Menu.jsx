0ebec69d96d9b1b53b2cdf3f387353717ecb039b
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-09-27 16:47:13 +0600

    Working with menu cart

--- client/src/components/home/Menu.jsx
+++ client/src/components/home/Menu.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import MenuCard from "./MenuCard";
+import Burger1 from "./../../assets/logo2.png";
 
 const Menu = () => {
+  const handlerAddToCart = (itemNum) => {};
+
   return (
     <section id="menu">
       <h1>Menu</h1>
-      <div>menuList</div>
+      <div>
+        <MenuCard
+          itemNum={1}
+          burgerSrc={Burger1}
+          price={200}
+          title="Cheese Burger"
+          handlerBuyButton={handlerAddToCart}
+        />
+        <MenuCard
+          itemNum={2}
+          burgerSrc={Burger1}
+          price={300}
+          title="Chill Burger"
+          handlerBuyButton={handlerAddToCart}
+        />
+        <MenuCard
+          itemNum={3}
+          burgerSrc={Burger1}
+          price={400}
+          title="Beef Burger"
+          handlerBuyButton={handlerAddToCart}
+        />
+      </div>
     </section>
   );
 };
--- client/src/components/home/MenuCard.jsx
+++ client/src/components/home/MenuCard.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const MenuCard = ({ itemNum }) => {
+const MenuCard = ({ itemNum, burgerSrc, price, title, handlerBuyButton }) => {
   return (
     <>
       <motion.div className="menuCard">
         <div>Item {itemNum}</div>
         <main>
-          <img src="" alt="" />
+          <img src={burgerSrc} alt="burgerImg" />
+          <h5>$ {price}</h5>
+          <p>{title}</p>
+          <button onClick={() => handlerBuyButton(itemNum)}>Buy Now</button>
         </main>
       </motion.div>
     </>
