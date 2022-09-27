d526a3e85e78fa64571f31d4e09d4b9493fe9ca2
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-09-27 21:38:18 +0600

    Working with cart

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/home/Home";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Contact from "./components/contact/Contact";
+import Cart from "./components/cart/Cart";
 import "./styles/app.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
@@ -10,6 +11,7 @@ import "./styles/founder.scss";
 import "./styles/menu.scss";
 import "./styles/footer.scss";
 import "./styles/contact.scss";
+import "./styles/cart.scss";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
 
       {/* Footer */}
---
+++ client/src/components/cart/Cart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cart = () => {
+  return <section className="cartSection">Add to cart</section>;
+};
+
+export default Cart;
