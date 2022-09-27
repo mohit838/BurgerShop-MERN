a40aab8ff304e87b8bd2f183699b56a4dc9ed8f2
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-09-27 10:46:12 +0600

    Add founder

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Founder from "./Founder";
 
 const Home = () => {
   const motionOp = {
@@ -8,22 +9,25 @@ const Home = () => {
   };
 
   return (
-    <section className="home">
-      <div>
-        <motion.h1 {...motionOp}>Burger Shop</motion.h1>
-        <motion.p {...motionOp} transition={{ delay: "0.2" }}>
-          Give Yourself A Tasty Treat
-        </motion.p>
-      </div>
-      <motion.a
-        href="#menu"
-        initial={{ y: "-100%", opacity: "0" }}
-        whileInView={{ y: "0", opacity: "1" }}
-        transition={{ delay: "0.4" }}
-      >
-        Explore Your Treat
-      </motion.a>
-    </section>
+    <>
+      <section className="home">
+        <div>
+          <motion.h1 {...motionOp}>Burger Shop</motion.h1>
+          <motion.p {...motionOp} transition={{ delay: "0.2" }}>
+            Give Yourself A Tasty Treat
+          </motion.p>
+        </div>
+        <motion.a
+          href="#menu"
+          initial={{ y: "-100%", opacity: "0" }}
+          whileInView={{ y: "0", opacity: "1" }}
+          transition={{ delay: "0.4" }}
+        >
+          Explore Your Treat
+        </motion.a>
+      </section>
+      <Founder />
+    </>
   );
 };
 
