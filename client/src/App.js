bb8105d4be6a0c9769d038a44a0aa172643d2294
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-09-27 10:46:59 +0600

    Founder style add

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/layout/Header";
 import "./styles/app.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
+import "./styles/founder.scss";
 
 function App() {
   return (
