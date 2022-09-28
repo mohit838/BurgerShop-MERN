a2cafdb6c0713c378eb2d6fa5e99326197273ceb
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-09-28 11:29:23 +0600

    Working with shipping ui

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Contact from "./components/contact/Contact";
 import Cart from "./components/cart/Cart";
+import Shipping from "./components/cart/Shipping";
 import "./styles/app.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
@@ -12,6 +13,7 @@ import "./styles/menu.scss";
 import "./styles/footer.scss";
 import "./styles/contact.scss";
 import "./styles/cart.scss";
+import "./styles/shipping.scss";
 
 function App() {
   return (
@@ -21,6 +23,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/shipping" element={<Shipping />} />
       </Routes>
 
       {/* Footer */}
