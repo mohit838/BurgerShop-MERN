865d92da56bc7af44511a87f4d1fb5fd2e0d1d57
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-09-28 11:18:21 +0600

    Complete Cart

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,52 +1,71 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Burger1 from "./../../assets/logo2.png";
 
-const CartItems = ({ value, title, image, increment, decrement }) => {
-  return (
-    <>
-      <div className="cardItem">
-        <div>
-          <h4>{title}</h4>
-          <img src={image} alt="BurgerCart" />
-        </div>
+const CartItems = ({ value, title, img, increment, decrement }) => (
+  <div className="cartItemCard">
+    <div>
+      <h4>{title}</h4>
+      <img src={img} alt="Item" />
+    </div>
 
-        {/* Button */}
-        <button onClick={decrement}>-</button>
-        <input type="number" readOnly value={value} />
-        <button onClick={increment}>+</button>
-      </div>
-    </>
-  );
-};
+    <div>
+      <button onClick={decrement}>-</button>
+      <input type="number" readOnly value={value} />
+      <button onClick={increment}>+</button>
+    </div>
+  </div>
+);
 
 const Cart = () => {
   const increment = (item) => {};
+
   const decrement = (item) => {};
 
   return (
     <section className="cartSection">
       <main>
         <CartItems
+          title={"Veg Burger"}
+          img={Burger1}
           value={0}
-          title={"Beef Burger"}
-          image={Burger1}
           increment={() => increment(1)}
           decrement={() => decrement(1)}
         />
         <CartItems
-          value={2}
-          title={"Cheese Burger"}
-          image={Burger1}
+          title={"Beef Burger"}
+          img={Burger1}
+          value={0}
           increment={() => increment(2)}
           decrement={() => decrement(2)}
         />
         <CartItems
-          value={3}
-          title={"Special Veg Burger"}
-          image={Burger1}
+          title={"Cheese Burger"}
+          img={Burger1}
+          value={0}
           increment={() => increment(3)}
           decrement={() => decrement(3)}
         />
+
+        <article>
+          <div>
+            <h4>Sub Total</h4>
+            <p>$ {2000}</p>
+          </div>
+          <div>
+            <h4>Tax</h4>
+            <p>$ {2000 * 0.18}</p>
+          </div>
+          <div>
+            <h4>Shipping Charges</h4>
+            <p>$ {200}</p>
+          </div>{" "}
+          <div>
+            <h4>Total</h4>
+            <p>$ {2000 + 2000 * 0.18 + 200}</p>
+          </div>
+          <Link to="/shipping">Checkout</Link>
+        </article>
       </main>
     </section>
   );
