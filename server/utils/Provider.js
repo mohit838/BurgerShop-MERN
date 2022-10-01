7e21612aea6377c8772fb4ac46356dd317239b4c
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-01 12:13:21 +0600

    Update Provider

--- server/utils/Provider.js
+++ server/utils/Provider.js
@@ -3,10 +3,24 @@ import passport from "passport";
 
 export const connectPassport = () => {
   passport.use(
-    new GoogleStrategy({
-      clientID: "rio",
-      clientSecret: "rio",
-      callbackURL: "rio",
-    })
+    new GoogleStrategy(
+      {
+        clientID: "rio",
+        clientSecret: "rio",
+        callbackURL: "rio",
+      },
+      async function (accessToken, refreshToken, profile, next) {
+        // Database Has Need to be here
+      }
+    )
   );
 };
+
+passport.serializeUser((user, next) => {
+  next(null, user.id);
+});
+
+passport.deserializeUser(async (id, next) => {
+  //   const user = await User.findById(id);
+  next(null, user);
+});
