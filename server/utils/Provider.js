7bd3c721f44bcc00de5e906f5b1cd3a196d0b736
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-01 13:02:18 +0600

    Update Provider Js

--- server/utils/Provider.js
+++ server/utils/Provider.js
@@ -1,26 +1,42 @@
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import passport from "passport";
+import { User } from "../models/User.js";
 
 export const connectPassport = () => {
   passport.use(
     new GoogleStrategy(
       {
-        clientID: "rio",
-        clientSecret: "rio",
-        callbackURL: "rio",
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
       },
-      async function (accessToken, refreshToken, profile, next) {
-        // Database Has Need to be here
+      async function (accessToken, refreshToken, profile, done) {
+        // User has been in DB
+        const user = await User.findOne({
+          googleId: profile.id,
+        });
+
+        // User not in DB
+        if (!user) {
+          const newUser = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            photo: profile.photos[0].value,
+          });
+
+          return done(null, newUser);
+        } else {
+          return done(null, user);
+        }
       }
     )
   );
+  passport.serializeUser((user, next) => {
+    next(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, next) => {
+    const user = await User.findById(id);
+    next(null, user);
+  });
 };
-
-passport.serializeUser((user, next) => {
-  next(null, user.id);
-});
-
-passport.deserializeUser(async (id, next) => {
-  //   const user = await User.findById(id);
-  next(null, user);
-});
