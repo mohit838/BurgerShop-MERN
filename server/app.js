ccbbdb9d1e778ff108c46567128838b86a309307
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-02 12:21:34 +0600

    Import passport

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectPassport } from "./utils/Provider.js";
 import session from "express-session";
+import passport from "passport";
 
 const app = express();
 
@@ -19,6 +20,10 @@ app.use(
     saveUninitialized: false,
   })
 );
+
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // PassportGoogle Connections
 connectPassport();
