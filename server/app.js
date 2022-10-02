9323baa40f8839b94a99aebd40fbd39117c0cda4
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-02 12:17:05 +0600

    Session Secret Create

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectPassport } from "./utils/Provider.js";
+import session from "express-session";
 
 const app = express();
 
@@ -9,6 +10,15 @@ export default app;
 dotenv.config({
   path: "./config/.env",
 });
+
+// Using Middlewares
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // PassportGoogle Connections
 connectPassport();
