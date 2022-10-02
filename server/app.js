b5048a4627d19aca199cf8c884748d9be8533e40
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-02 17:43:55 +0600

    Add Cookie Parser

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectPassport } from "./utils/Provider.js";
 import session from "express-session";
 import passport from "passport";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -22,6 +23,8 @@ app.use(
     saveUninitialized: false,
   })
 );
+
+app.use(cookieParser());
 
 app.use(passport.authenticate("session"));
 app.use(passport.initialize());
