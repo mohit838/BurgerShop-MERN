9259e659ff28a0b15e509671028b8ba1c3beb9d1
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-02 12:50:23 +0600

    Redirect Login API Create

--- server/routes/users.js
+++ server/routes/users.js
@@ -12,9 +12,15 @@ router.get(
 
 router.get(
   "/login",
-  passport.authenticate("google", {
-    successRedirect: process.env.FRONTEND_URL,
-  })
+  (req, res, next) => {
+    res.send("Logged In");
+  }
+
+  // Fot Temporary Commenting
+  // passport.authenticate("google", {
+  //   scope: ["profile"],
+  //   successRedirect: process.env.FRONTEND_URL,
+  // })
 );
 
 export default router;
